935ab8ba9527183c1818dde48f4aa963d6c34e5f
Tevon Talbert <TevonTalbert@hotmail.com>
2023-10-02 19:08:33 -0500

    added

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+    let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4;
+let num4 = 13;
+const mod = num3 % num4;
+
+let num5 = 10;
+let num6 = 15;
+let num7 = 20;
+const max = Math.max(num5, num6, num7);
